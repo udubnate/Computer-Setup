049ad4b5fcf162bde123885a3c0e522ec9be6e7e
udubnate <natebruneau@gmail.com>
2025-04-11 09:33:02 -0700

    feat: add setup script for essential tools installation
    - 🛠️ Install essential tools and utilities
    - 🖥️ Install development tools
    - 🎵 Install media and entertainment tools
    - 🌐 Install remote access tools
    - 🔄 Update WSL (Windows Subsystem for Linux)
    - 🚀 Install Starship prompt for testing purposes

---
+++ Windows/Setup-Programs.ps1
@@ -0,0 +1,21 @@
+# Install essential tools and utilities
+winget install Microsoft.PowerToys
+winget install Obsidian.Obsidian
+winget install Microsoft.VisualStudioCode
+winget install Microsoft.PowerShell
+
+# Install development tools
+winget install Docker.DockerDesktop
+winget install Git.Git
+
+# Install media and entertainment tools
+winget install Spotify.Spotify
+
+# Install remote access tools
+winget install Microsoft.RemoteDesktopClient
+
+# Update WSL (Windows Subsystem for Linux)
+Wsl --update
+
+# Install Starship prompt (for testing purposes)
+winget install --id=Starship.Starship -e
